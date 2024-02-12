cd495f1f01d642c8bbbc91c69ed5759ab54ff444
Felix Amenya <kenyansafelix@gmail.com>
2024-02-12 22:00:08 +0300

    lazygrid

---
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.littlelemon
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.littlelemon.ui.theme.LittleLemonTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LittleLemonTheme {
+                AppScreen()
+            }
+        }
+        }
+    }
+
+    @Composable
+    private fun AppScreen() {
+        LazyGrid()
+    }
+
